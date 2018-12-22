66e46ddc75419a6e693399c0319135fbb55e4669
Tim Leland <timothy.leland@gmail.com>
2018-12-22 14:18:30 -0500

    Bug fixes

--- Skycon.vue
+++ Skycon.vue
@@ -1,65 +1,67 @@
 <template>
-<canvas :id="id" :width="width" :height="height"></canvas>
+	<canvas :id="id" :width="width" :height="height"></canvas>
 </template>
 
 <script>
-import Skycons from './skycons';
+import Skycons from "./skycons";
 
 export default {
-    props: {
-        width: {
-            type: [String, Number],
-            default: 64
-        },
-        height: {
-            type: [String, Number],
-            default: 64
-        },
-        condition: {
-            type: String
-        },
-        color: {
-            type: String,
-            default: 'black'
-        }
-    },
-    data: function () {
-        return {
-            id: 'canvas' + Date.now().valueOf() + Math.floor(10000 * Math.random()),
-            skycons: null
-        };
-    },
-    created() {
-        this.create();
-    },
-    methods: {
-        create() {
-            // initialize skycons
-            this.destroy();
-            this.skycons = new Skycons({
-                color: this.color
-            });
-            this.skycons.play();
-        },
-        destroy() {
-            if (this.id && this.skycons) {
-                this.skycons.remove(this.id);
-            }
-        }
-    },
-    watch: {
-        condition(value) {
-            value = value.toUpperCase().replace(/-/g, '_');
-            if (this.skycons) {
-                this.skycons.set(this.id, Skycons[value]);
-            }
-        },
-        options(value) {
-            this.create(value);
-        }
-    },
-    destroyed() {
-        this.destroy();
-    }
+	props: {
+		width: {
+			type: [String, Number],
+			default: 64
+		},
+		height: {
+			type: [String, Number],
+			default: 64
+		},
+		condition: {
+			type: String
+		},
+		color: {
+			type: String,
+			default: "black"
+		}
+	},
+	data: function() {
+		return {
+			id: "canvas" + this._uid,
+			skycons: null
+		};
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			// initialize skycons
+			this.destroy();
+			this.skycons = new Skycons({
+				color: this.color
+			});
+
+			this.setCondition(this.condition);
+			this.skycons.play();
+		},
+		setCondition(value) {
+			value = value.toUpperCase().replace(/-/g, "_");
+			if (this.skycons) {
+				this.skycons.set(this.id, Skycons[value]);
+			}
+		},
+		destroy() {
+			if (this.id && this.skycons) {
+				this.skycons.remove(this.id);
+			}
+		}
+	},
+	watch: {
+		condition(value) {
+			this.setCondition(value);
+		}
+	},
+	destroyed() {
+		this.destroy();
+	}
 };
 </script>
